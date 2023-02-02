356e37b1f6cc0eff770f976d8079648c753b2546
igor <blackstanislav@gmail.com>
2023-02-02 16:22:14 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+
+        int cost = 1000;
+        int rubles = 20;
+
+        int bonus_miles = cost / rubles;
+        System.out.println("Бесплатные мили: " + bonus_miles);
+    }
+}
